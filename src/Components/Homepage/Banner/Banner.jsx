7ca69ed923709e74c5da652ec3945f7a9c266c36
Source-Code-007 <utsho926@gmail.com>
2023-06-13 16:29:45 +0600

    initial setup complete

---
+++ src/Components/Homepage/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+    return (
+        <div className="min-h-screen bg-red-500">
+            Thisi banner
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Layout/LayoutOne.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router-dom';
+
+const LayoutOne = () => {
+    return (
+        <Outlet></Outlet>
+    );
+};
+
+export default LayoutOne;
