49a066ed64ef89ec4b7ff0ca376c31a22a195407
Source-Code-007 <utsho926@gmail.com>
2023-06-14 16:08:43 +0600

    social link added in banner

--- src/Components/Homepage/Banner/Banner.jsx
+++ src/Components/Homepage/Banner/Banner.jsx
@@ -2,10 +2,12 @@
 import { TypeAnimation } from 'react-type-animation';
 import testBg2 from '../../../assets/img/wave-haikei.svg'
 import { motion } from "framer-motion";
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Banner = () => {
     return (
-        <div className="min-h-screen bg-cover bg-center flex items-center justify-center text-white bg-fixed" style={{ backgroundImage: `url(${testBg2})` }}>
+        <div className="min-h-screen bg-cover bg-center flex items-center justify-center text-white bg-fixed relative" style={{ backgroundImage: `url(${testBg2})` }}>
             <div className='pl-14 px-5 w-full md:4/6 xl:w-3/6'>
                 <motion.div
                     animate={{ x: 50 }}
@@ -42,6 +44,11 @@ const Banner = () => {
                         Hire me
                     </motion.button>
                 </motion.div>
+            </div>
+            <div className='w-auto h-auto flex flex-col gap-5 text-xl absolute right-5 top-1/2 -translate-x-1/2 bg-[#e74d3c55] p-3'>
+                <Link className='inline-block focus:scale-110 text-[#e74c3c]'><FaFacebook></FaFacebook></Link>
+                <Link className='inline-block focus:scale-110 text-[#e74c3c]'><FaLinkedin></FaLinkedin></Link>
+                <Link className='inline-block focus:scale-110 text-[#e74c3c]'><FaGithub></FaGithub></Link>
             </div>
         </div >
     );
